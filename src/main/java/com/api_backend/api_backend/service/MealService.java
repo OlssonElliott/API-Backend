3889fc15af676857586bd0c2f12e761af15246d1
Elliott <sendtoelle@hotmail.com>
2025-01-27 12:16:49 +0100

    connected controller/service/repository, also tested adding

--- src/main/java/com/api_backend/api_backend/service/MealService.java
+++ src/main/java/com/api_backend/api_backend/service/MealService.java
@@ -2,7 +2,29 @@ package com.api_backend.api_backend.service;
 
 import org.springframework.stereotype.Service;
 
+import com.api_backend.api_backend.model.Meal;
+import com.api_backend.api_backend.repository.MealRepository;
+
 @Service
 public class MealService {
+
+    MealRepository mealRepository;
+
+    public MealService(MealRepository mealRepository) {
+        this.mealRepository = mealRepository;
+    }
+
+    public void addMeal() {
+        Meal testmeal = new Meal(1, "source", "comment");
+        mealRepository.save(testmeal);
+    }
+
+    public void findMeal() {
+
+    }
+
+    public void removeMeal() {
+
+    }
 
 }
