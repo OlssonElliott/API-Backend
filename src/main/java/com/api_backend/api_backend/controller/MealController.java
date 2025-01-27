4f2c7f209dead669e73d16a926c07d28d9654432
Elliott <sendtoelle@hotmail.com>
2025-01-27 15:24:23 +0100

    fixed cors with CrossOrigin

--- src/main/java/com/api_backend/api_backend/controller/MealController.java
+++ src/main/java/com/api_backend/api_backend/controller/MealController.java
@@ -7,15 +7,20 @@ import com.api_backend.api_backend.service.MealService;
 
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+/*
+ * för att kunna nå localhost:8080 från 8081 fick jag googla lite, länk:
+ * https://spring.io/guides/gs/rest-service-cors
+ */
+@CrossOrigin(origins = "http://localhost:8081")
 public class MealController {
 
     MealService mealService;
