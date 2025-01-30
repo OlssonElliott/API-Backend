efc05b54342e9616120f7c2244ef797c139ad931
Elliott <sendtoelle@hotmail.com>
2025-01-30 22:09:08 +0100

    fixed update comment function

--- src/main/java/com/api_backend/api_backend/controller/MealController.java
+++ src/main/java/com/api_backend/api_backend/controller/MealController.java
@@ -7,15 +7,19 @@ import com.api_backend.api_backend.service.MealService;
 
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+
+// Fick använda @CrossOrigin för kunde inte nå 8080 pga CORS i frontend, källa:
+// https://spring.io/guides/gs/rest-service-cors
+@CrossOrigin(origins = "http://localhost:8081")
 public class MealController {
 
     MealService mealService;
@@ -54,6 +58,11 @@ public class MealController {
         } else {
             return "meal id: " + id + " is not a favorite";
         }
+    }
+
+    @PutMapping("/comment")
+    public String setComment(@RequestParam Integer id, @RequestParam String comment) {
+        return mealService.setComment(id, comment);
     }
 
 }
